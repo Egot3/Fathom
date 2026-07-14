a8e6cc2b4289e5a18c03a6d441b3d5af98625046
ETS <noobicklp@gmail.com>
2026-07-14 23:58:27 +0300

    feat(frontend): slug-start of router'

---
+++ application/web/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  const regex = /^http.\/\/.+?\/(.*)$/gm
+
+  let currentURL = $state('')
+
+  let slug = $derived(
+    regex.exec(currentURL)?.[1] ?? ""
+  )
+
+  onMount(()=>{
+    currentURL = window.location.href
+  })
+</script>
+
+<p class="bg-error-100">current url: {currentURL}</p>
+<p>slug: {slug}</p>
+<p style="background: #fbbf24;">a bunch of text</p>
