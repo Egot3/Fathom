d2e11bfd3931f50ce6b049d1e180a6491f301233
ETS <noobicklp@gmail.com>
2026-07-15 20:01:11 +0300

    feat(frontend): added simple routing and 404page

--- application/web/src/App.svelte
+++ application/web/src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { onMount } from "svelte";
+    import type { Component } from 'svelte';
+  import Login from "./pages/Login.svelte";
+  import Status404 from "./pages/Status404.svelte";
 
-  const regex = /^http.\/\/.+?\/(.*)$/gm
+  const regex = /^http.\/\/.+?\/(.*)$/
 
   let currentURL = $state('')
 
@@ -12,8 +15,15 @@
   onMount(()=>{
     currentURL = window.location.href
   })
+
+  const pages = new Map<string, Component>([
+    ["login", Login],
+  ])
 </script>
 
-<p class="bg-error-100">current url: {currentURL}</p>
-<p>slug: {slug}</p>
-<p style="background: #fbbf24;">a bunch of text</p>
+{#if pages.has(slug)}
+  {@const ActivePage = pages.get(slug)}
+  <ActivePage />
+{:else}
+  <Status404 />
+{/if}
---
+++ application/web/src/pages/Status404.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+</script>
+
+<div class="absolute grid w-full h-full items-center justify-center">
+
+<div class="flex flex-col items-center justify-center gap-y-5 z-30">
+    <p class="text-7xl">Page not found!</p>
+    <a type="button" class="btn text-2xl preset-outlined-error-500" href="/home">Take me home!</a>
+</div>
+    
+</div>
+
+
+<div class="absolute inset-0 grid place-items-center">
+  <div class="opacity-10 text-center text-pretty w-2/3 pointer-events-none select-none text-surface-500">
+    There was a problem loading your requested page, yet without it you might have never seen this text, this word, this letter, that sacret bit of pure information.
+    Yet if it existed, then what? What kind of knowledge would you posses then? Can we say, that knowledge of non-existance is equal to the one, explaining why it exists?
+    Or, in fact, this page actually exists, and you just can't get to it? Then send a letter to this address <a href="mailto:ets@etsevilcorp.com" class="pointer-events-auto z-2">ets@etsevilcorp.com</a>(clickable)
+    and don't forget to write how this happened. If you are COMPLETELY SURE that this is a bug, then create an issue on <a href="https://github.com/Egot3/fathom"  class="pointer-events-auto z-2">GitHub</a>(also clickable).
+    If you are completing a test at the moment, then my apolocheese for this poem, this empty space felt so... empty. There is surely a name for this in designer community, but I am not a
+    designer, I am a developer, preffer backend. Frontend is fun until it isn't. Why I haven't used lorem there, you might ask. I just wanted you to read it and understand, that this is
+    completely human-written text ;). And since you are already read this far, did you know that this project actually aimed to improve server's perfomance, but not overall?
+    Yet now, by caching and stuff, it simplifies proccess everywhere. At the time of writing this text, it's quite the pinicle of my abilities. 
+    There was a problem loading your requested page, yet without it you might have never seen this text, this word, this letter, that sacret bit of pure information.
+    Yet if it existed, then what? What kind of knowledge would you posses then? Can we say, that knowledge of non-existance is equal to the one, explaining why it exists?
+    Or, in fact, this page actually exists, and you just can't get to it? Then send a letter to this address <a href="mailto:ets@etsevilcorp.com"  class="pointer-events-auto z-2">ets@etsevilcorp.com</a>(clickable)
+    and don't forget to write how this happened. If you are COMPLETELY SURE that this is a bug, then create an issue on <a href="https://github.com/Egot3/fathom"  class="pointer-events-auto z-2">GitHub</a>(also clickable).
+    If you are completing a test at the moment, then my apolocheese for this poem, this empty space felt so... empty. There is surely a name for this in designer community, but I am not a
+    designer, I am a developer, preffer backend. Frontend is fun until it isn't. Why I haven't used lorem there, you might ask. I just wanted you to read it and understand, that this is
+    completely human-written text ;). And since you are already read this far, did you know that this project actually aimed to improve server's perfomance, but not overall?
+    Yet now, by caching and stuff, it simplifies proccess everywhere. At the time of writing this text, it's quite the pinicle of my abilities. 
+    There was a problem loading your requested page, yet without it you might have never seen this text, this word, this letter, that sacret bit of pure information.
+    Yet if it existed, then what? What kind of knowledge would you posses then? Can we say, that knowledge of non-existance is equal to the one, explaining why it exists?
+    Or, in fact, this page actually exists, and you just can't get to it? Then send a letter to this address <a href="mailto:ets@etsevilcorp.com"  class="pointer-events-auto z-2">ets@etsevilcorp.com</a>(clickable)
+    and don't forget to write how this happened. If you are COMPLETELY SURE that this is a bug, then create an issue on <a href="https://github.com/Egot3/fathom"  class="pointer-events-auto z-2">GitHub</a>(also clickable).
+    If you are completing a test at the moment, then my apolocheese for this poem, this empty space felt so... empty. There is surely a name for this in designer community, but I am not a
+    designer, I am a developer, preffer backend. Frontend is fun until it isn't. Why I haven't used lorem there, you might ask. I just wanted you to read it and understand, that this is
+    completely human-written text ;). And since you are already read this far, did you know that this project actually aimed to improve server's perfomance, but not overall?
+    Yet now, by caching and stuff, it simplifies proccess everywhere. At the time of writing this text, it's quite the pinicle of my abilities. 
+    
+    Yes, this is the same text repetead thrise, I got bored a bit
+  </div>
+</div>
