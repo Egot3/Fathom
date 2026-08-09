c6f92e0b6572dca58bc5e0ef4131bb9fd9bd59a7
ETS <noobicklp@gmail.com>
2026-08-09 22:45:18 +0300

    feat(frontend): added FetchTotals function

--- applications/application/web/src/lib/contracts/totals.ts
+++ applications/application/web/src/lib/contracts/totals.ts
@@ -2,61 +2,86 @@ import type { JSONError } from "../statuses/jsonerror";
 import { TokenizedFetch } from "./tokenizedFetch";
 
 type rawTotal = {
-	test_uuid: string;
-	group_uuid: string;
-	user_uuid: string;
-	score: number;
+  test_uuid: string;
+  group_uuid: string;
+  user_uuid: string;
+  score: number;
 };
 
 export type TestTotal = {
-	test_name: string;
-	group_name: string;
-	user_name: string;
-	score: number;
+  test_name: string;
+  group_name: string;
+  user_name: string;
+  score: number;
 
-	test_uuid: string;
-	group_uuid: string;
-	user_uuid: string;
+  test_uuid: string;
+  group_uuid: string;
+  user_uuid: string;
 };
 
 export type TotalsOrError = { totals: TestTotal[]; total: number } | JSONError;
 
 export async function GetTotalsForUser(
-	userUUID: string,
-	page: number,
-	size: number,
+  userUUID: string,
+  page: number,
+  size: number,
 ): Promise<TotalsOrError> {
-	try {
-		const rawRes = await TokenizedFetch(
-			"https://" +
-				import.meta.env.VITE_DOMAIN +
-				"/api/v1/total/all/" +
-				userUUID +
-				"?page=" +
-				page +
-				"&size=" +
-				size,
-			{
-				method: "GET",
-				headers: {
-					Accept: "application/json",
-				},
-			},
-		);
+  try {
+    const rawRes = await TokenizedFetch(
+      `https://${import.meta.env.VITE_DOMAIN}/api/v1/total/all/${userUUID}?page=${page}&size=${size}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      },
+    );
 
-		if (!rawRes.ok) {
-			return (await rawRes.json()) as JSONError;
-		}
+    if (!rawRes.ok) {
+      return (await rawRes.json()) as JSONError;
+    }
 
-		const totals = (await rawRes.json()) as {
-			totals: TestTotal[];
-			total: number;
-		};
-		return totals;
-	} catch (err) {
-		console.log(err);
-		return {
-			error: "network error",
-		} as JSONError;
-	}
+    const totals = (await rawRes.json()) as {
+      totals: TestTotal[];
+      total: number;
+    };
+    return totals;
+  } catch (err) {
+    console.log(err);
+    return {
+      error: "network error",
+    } as JSONError;
+  }
+}
+
+export async function FetchTotals(
+  page: number,
+  size: number,
+): Promise<TotalsOrError> {
+  try {
+    const rawRes = await TokenizedFetch(
+      `https://${import.meta.env.VITE_DOMAIN}/api/v1/total?page=${page}&size=${size}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      },
+    );
+
+    if (!rawRes.ok) {
+      return (await rawRes.json()) as JSONError;
+    }
+
+    const totals = (await rawRes.json()) as {
+      totals: TestTotal[];
+      total: number;
+    };
+    return totals;
+  } catch (err) {
+    console.log(err);
+    return {
+      error: "network error",
+    } as JSONError;
+  }
 }
