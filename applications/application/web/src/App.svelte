ede6a8d43e72fb2f1a81b75a06b7802916a32526
ETS <noobicklp@gmail.com>
2026-08-29 18:43:57 +0300

    feat(frontend): optimized initial load

--- applications/application/web/src/App.svelte
+++ applications/application/web/src/App.svelte
@@ -1,56 +1,60 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import type { Component } from "svelte";
-	import Login from "./pages/Login.svelte";
-	import Status404 from "./pages/Status404.svelte";
-	import Register from "./pages/Register.svelte";
-	import Home from "./pages/Home.svelte";
-	import { GetUser } from "./lib/bgdata/user.svelte";
-	import Treachery from "./pages/Treachery.svelte";
+  import { onMount } from "svelte";
+  import type { Component } from "svelte";
+  import Status404 from "./pages/Status404.svelte";
+  import { GetUser } from "./lib/bgdata/user.svelte";
+  import _ from "lodash";
 
-	const regex = /^https?:\/\/[^\/]+\/([^?#]+)/;
+  const regex = /^https?:\/\/[^\/]+\/([^?#]+)/;
 
-	let currentURL = $state("");
+  let currentURL = $derived(window.location.href);
 
-	let hier_part = $derived(regex.exec(currentURL)?.[1] ?? "");
+  let hier_part = $derived(regex.exec(currentURL)?.[1] ?? "");
 
-	$inspect(hier_part);
+  $inspect(hier_part);
+  type LazyPage = () => Promise<{ default: Component<any, any, any> }>;
 
-	onMount(() => {
-		currentURL = window.location.href;
-	});
-	const pages = new Map<string, Component>([
-		["login", Login],
-		["register", Register],
-		["home", Home],
-		["teachery", Treachery],
-	]);
+  const pages = new Map<string, LazyPage>([
+    ["login", () => import("./pages/Login.svelte")],
+    ["register", () => import("./pages/Register.svelte")],
+    ["home", () => import("./pages/Home.svelte")],
+    ["teachery", () => import("./pages/Treachery.svelte")],
+  ]);
 
-	const publicRoutes = new Set(["login", "register"]);
-	const teacherRoutes = new Set(["teachery"]);
+  const publicRoutes = new Set(["login", "register"]);
+  const teacherRoutes = new Set(["teachery"]);
 
-	const sendToLogin = () => {
-		window.location.href = "/login";
-	};
-	const sendToHome = () => {
-		window.location.href = "/home";
-	};
-
-	console.log(GetUser());
+  const sendToLogin = () => {
+    window.location.href = "/login";
+  };
+  const sendToHome = () => {
+    window.location.href = "/home";
+  };
+  async function resolvePage(key: string) {
+    const loader = pages.get(key);
+    if (!loader) return null;
+    const { default: PageComponent } = await loader();
+    return PageComponent;
+  }
 </script>
 
-{#if pages.has(hier_part)}
-	{@const requestor = GetUser()}
-	{#if requestor == null && !publicRoutes.has(hier_part)}
-		{sendToLogin()}
-	{:else}
-		{#if !requestor?.isTeacher && teacherRoutes.has(hier_part)}
-			{sendToHome()}
-		{:else}
-			{@const ActivePage = pages.get(hier_part)}
-			<ActivePage />
-		{/if}
-	{/if}
+{const requestor = GetUser()}
+{#if requestor === null && !publicRoutes.has(hier_part)}
+  {sendToLogin()}
 {:else}
-	<Status404 />
+  {#if !requestor?.isTeacher && teacherRoutes.has(hier_part)}
+    {sendToHome()}
+  {:else}
+    {const ap = resolvePage(hier_part)}
+
+    {#await ap then ActivePage}
+      {#if ActivePage === null}
+        {console.log("is null")}
+        <Status404 />
+      {:else}
+        {console.log("is not null")}
+        <ActivePage />
+      {/if}
+    {/await}
+  {/if}
 {/if}
