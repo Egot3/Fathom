29ea4e271bbb6d2ba9e219d56a29534e5a36f42e
ETS <noobicklp@gmail.com>
2026-09-19 19:25:05 +0300

    fix(frontend): refactored chip selector to work with ids and not values

--- applications/application/web/src/components/ChipSelector.svelte
+++ applications/application/web/src/components/ChipSelector.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
   import { CheckIcon } from "@lucide/svelte";
 
-  const {
+  let {
     options = [] as string[],
     selected = $bindable(0),
     working = false,
   } = $props();
 
-  let color = $derived(options[selected]);
-
-  function setColor(selectedColor: string) {
-    color = selectedColor;
+  function setColor(selectedColorId: number) {
+    selected = selectedColorId;
   }
 
   $inspect(working);
 </script>
 
-{#each options as c (c)}
+{#each options as c, i (i)}
   <button
-    class={`chip capitalize preset-outlined-surface-400-600 ${color === c && !working ? "preset-tonal-primary" : ""} ${working ? "preset-tonal-warning animate-pulse" : ""}`}
-    onclick={() => setColor(c)}
+    class={`chip capitalize preset-outlined-surface-400-600 ${selected === i && !working ? "preset-tonal-primary" : ""} ${working ? "preset-tonal-warning animate-pulse" : ""}`}
+    onclick={() => setColor(i)}
     disabled={working}
   >
-    {#if color === c}<CheckIcon size={14} />{/if}
+    {#if selected === i}<CheckIcon size={14} />{/if}
     <span>{c}</span>
   </button>
 {/each}
